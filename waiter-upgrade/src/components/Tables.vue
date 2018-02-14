5feff75c5b7435c312f04ca898a7d0d2c833b492
Vladimir Curcic <vladimircurcic@canicinteractive.com>
2018-02-14 17:03:45 +0100

    Testing routes

---
+++ waiter-upgrade/src/components/Tables.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    GFFFF
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "tables"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- waiter-upgrade/src/router/index.js
+++ waiter-upgrade/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Tables from '@/components/Tables'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Create',
+      component: HelloWorld, Tables
     }
   ]
 })
